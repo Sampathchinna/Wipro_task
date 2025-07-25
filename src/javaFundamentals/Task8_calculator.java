8592b823c7d8de585ce56b219a5bfee4d1ab99e7
sampath <samchinna1563@gmail.com>
2025-07-25 18:05:53 +0530

    Update Task8_calculator.java

--- src/javaFundamentals/Task8_calculator.java
+++ src/javaFundamentals/Task8_calculator.java
@@ -2,53 +2,52 @@ package javaFundamentals;
 import java.util.Scanner;
 
 public class Task8_calculator {
-	 public static void main(String[] args) {
-	        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	        System.out.print("Enter first number: ");
-	        int a = scanner.nextInt();
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
 
-	        System.out.print("Enter second number: ");
-	        int b = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        
+        scanner.nextLine();
+        
+        System.out.println("1.Addition");
+        System.out.println("2.Subtraction");
+        System.out.println("3.Multiplication");
+        System.out.println("Division");
+        System.out.print("Enter your choice: ");
+        int choice=scanner.nextInt();
+        
+        
+        
+        switch (choice) {
+        
+        case 1:
+        	int sum=a+b;
+	        System.out.println("Addition:" + sum);
+	        break;
 	        
-	        scanner.nextLine();
-	        
-	        System.out.println("Select your operator:Addition,Subtraction,Multiplication,Division");
-	        String opertaor=scanner.nextLine();
-	        
-	        
-	        
-	        switch (opertaor) {
-	        
-	        case "Addition":
-	        	int sum=a+b;
-		        System.out.println("Addition:" + sum);
-		        break;
-		        
-	        case "Subtraction":
-	        	int diff = a - b;
-		        System.out.println("Subtraction:" + diff);
-		        break;
-		    
-		    case "Multiplication":
-		    	int product = a * b;
-		    	System.out.println("Multiplication:" + product);
-		    	break;
-		    	
-		    case "Division":
-		    	 int quotient = a / b;
-		    	 System.out.println("Division:" + quotient);
-		    	 break;
-		    
-		    case "Remainder":
-		    	  int remainder = a % b;
-		    	  System.out.println("Modulus:" + remainder);
-		    	  break;
-		    	
-		    default:
-		    	System.out.println("Please select a valid operator");
-		    	break;
-	        	
-	        }     
-	    }
-	}
+        case 2:
+        	int diff = a - b;
+	        System.out.println("Subtraction:" + diff);
+	        break;
+	    
+	    case 3:
+	    	int product = a * b;
+	    	System.out.println("Multiplication:" + product);
+	    	break;
+	    	
+	    case 4:
+	    	 int quotient = a / b;
+	    	 System.out.println("Division:" + quotient);
+	    	 break;
+	    	
+	    default:
+	    	System.out.println("Please select a valid operator");
+	    	break;
+        	
+        }     
+    }
+}
