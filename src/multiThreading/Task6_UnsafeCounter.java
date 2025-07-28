38066b0ad2853cd79f1bad6e83b73105c08ef909
sampath <samchinna1563@gmail.com>
2025-07-28 16:30:06 +0530

    Update Task6_UnsafeCounter.java

--- src/multiThreading/Task6_UnsafeCounter.java
+++ src/multiThreading/Task6_UnsafeCounter.java
@@ -1,23 +1,23 @@
 package multiThreading;
 
-public class Task6_UnsafeCounter {
-    static int count = 0;
-
-    public static void main(String[] args) throws InterruptedException {
-        Runnable r = () -> {
-            for (int i = 0; i < 1000; i++) {
-                count++;
-            }
-        };
-
-        Thread t1 = new Thread(r);
-        Thread t2 = new Thread(r);
-        t1.start();
-        t2.start();
-        t1.join();
-        t2.join();
-
-        System.out.println("Final Count (may be < 2000): " + count);
-    }
-}
-
+class Counter {
+	 int count = 0;
+	 void increment() {
+	 count++;
+	 }
+	}
+	public class Task6_UnsafeCounter {
+	 public static void main(String[] args) throws InterruptedException {
+	 Counter c = new Counter();
+	 Thread t1 = new Thread(() -> {
+	 for(int i = 0; i < 1000; i++) c.increment();
+	 });
+	 Thread t2 = new Thread(() -> {
+	 for(int i = 0; i < 1000; i++) c.increment();
+	 });
+	 t1.start();
+	 t2.start();
+	 t1.join();
+	 t2.join();
+	 System.out.println("Final Count (without sync): " + c.count); // likely < 2000
+	 }}
